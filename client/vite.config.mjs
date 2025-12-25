f05281a6a0b426b56291d085ae777bc65561a566
Shakib <your_email@example.com>
2025-12-25 19:03:32 +0600

    remove vercel.json, use Vercel Vite SPA handling

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build"  // default dist থেকে change করলাম build
-  }
+    outDir: 'build', // এটা ঠিক আছে কি না দেখুন
+  },
+  base: './' // optional, relative path use করলে কখনও কখনও help করে
 });
